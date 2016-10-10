3a2102198878909a35f87b81fd276287d3298446
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-10 11:54:34 -0400

    add app component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,1 +1,14 @@
-// This document will set up App React Component
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app-component">
+        <p> App is rendering</p>
+      </div>
+    );
+  }
+}
+
+export default App;
