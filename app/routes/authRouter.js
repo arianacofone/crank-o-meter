098197338c49a6f27c2f09e1a186c8ef76ec7e39
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-11 10:05:56 -0400

    set up authRouter between Authenication and AuthController components

--- app/routes/authRouter.js
+++ app/routes/authRouter.js
@@ -1,1 +1,10 @@
-// This file will set up routes for the authentication process
+const express = require('express');
+const AuthController = require('../controllers/AuthController');
+
+const router = express.Router();
+
+router.post('/login', AuthController.login);
+router.post('/signup', AuthController.signUp);
+router.post('/signout', AuthController.signout);
+
+module.exports = router;
