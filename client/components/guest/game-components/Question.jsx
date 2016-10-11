811075167c6d4e2deb228f8bd277ab606ca2be85
thomas bongiorno <tbongiorno1@gmail.com>
2016-10-11 08:43:42 -0400

    basic question functionality

--- client/components/guest/game-components/Question.jsx
+++ client/components/guest/game-components/Question.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 
 class Question extends Component {
+  constructor() {
+    super();
+    this.handleAnswerClick = this.handleAnswerClick.bind(this);
+  }
+  handleAnswerClick() {
+    const newQuestion = document.createElement('div');
+    newQuestion.setAttribute('id', 'questionWindow');
+    newQuestion.innerHTML = `
+    <div id="question">
+      <p> Question </p>
+      <div id="question-text">the text of the question goes here</div>
+      <ul>
+      <li id="answerOne">answer one</li>
+      <li id="answerTwo">answer two</li>
+      <li id="answerThree">answer three</li>
+      </ul>
+    </div>
+    `
+    document.body.appendChild(newQuestion);
+  }
   render() {
     return (
       <div id="question">
-        <div id="question-text">the text of the question goes here</div>
         <p> Question </p>
-        <div id="answerOne">answer one</div>
-        <div id="answerTwo">answer two</div>
-        <div id="answerThree">answer three</div>
+        <div id="question-text">the text of the question goes here</div>
+        <ul>
+        <li id="answerOne" onClick={this.handleAnswerClick}>answer one</li>
+        <li id="answerTwo" onClick={this.handleAnswerClick}>answer two</li>
+        <li id="answerThree" onClick={this.handleAnswerClick}>answer three</li>
+        </ul>
       </div>
     );
   }
