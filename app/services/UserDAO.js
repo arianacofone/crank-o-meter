0710ba8a8772bd0e76c040dba0bff0ea47a184ed
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-11 12:08:55 -0400

    create userDAO functionality

--- app/services/UserDAO.js
+++ app/services/UserDAO.js
@@ -1,1 +1,22 @@
-// This file will set up the User DAO communication
+const db = require('../config/sql/db');
+const sql = require('../config/sql/sqlProvider').users;
+const User = require('../models/Users');
+
+class UserDAO {
+  static all() {
+    return db.map(sql.all, [], (row) => new User(row));
+  }
+  static findBy(keyValue) {
+    const key = Object.keys(keyValue)[0];
+    const value = keyValue[key];
+    return db.one(sql.find, [key, value])
+             .then((row) => new User(row));
+  }
+  staticCreate({ name, email, password }) {
+    return db.one(sql.create, [name, email, password])
+             . then((row) => new User(row));
+  }
+  // In future, add delete static function to remove sentences
+}
+
+module.exports = UserDAO;
