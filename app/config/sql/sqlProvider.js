b070a25d11bf946074d1bf92454eca2a5dfff1e9
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-11 12:18:05 -0400

    set up sqlProvider functionality

--- app/config/sql/sqlProvider.js
+++ app/config/sql/sqlProvider.js
@@ -1,1 +1,18 @@
-// This file will set up user functions associated with sql commands
+const QueryFile = require('pg-promise').QueryFile;
+const path = require('path');
+
+function sql(file) {
+  const fullPath = path.join(__dirname, file);
+  return new QueryFile(fullPath, { minify: true });
+}
+
+const sqlProvider = {
+  users: {
+    all: sql('./user/all.sql'),
+    create: sql('./user/create.sql'),
+    find: sql('./user/find.sql'),
+  },
+  //This is where sentence and letter functs will be added
+};
+
+module.exports = sqlProvider;
