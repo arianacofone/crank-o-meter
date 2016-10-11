b14b9f338bbb2f41cc72e6c342943ca8591890a2
thomas bongiorno <tbongiorno1@gmail.com>
2016-10-11 08:33:31 -0400

    css mock up

--- client/components/guest/game-components/Question.jsx
+++ client/components/guest/game-components/Question.jsx
@@ -4,7 +4,11 @@ class Question extends Component {
   render() {
     return (
       <div id="question">
+        <div id="question-text">the text of the question goes here</div>
         <p> Question </p>
+        <div id="answerOne">answer one</div>
+        <div id="answerTwo">answer two</div>
+        <div id="answerThree">answer three</div>
       </div>
     );
   }
