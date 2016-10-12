0c70f9e4a6e6bcee50070baf1b304c04031e3a7f
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-12 11:41:23 -0400

    begin to create sentence api functionality

--- client/components/member/letter-components/Letter.jsx
+++ client/components/member/letter-components/Letter.jsx
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 import Sentence from './Sentence.jsx';
+import Home from './Home.jsx';
 
 class Letter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sentence: [],
+    };
+    this.getSentence = this.getSentence.bind(this);
+    this.sendSentence = this.sendSentence.bind(this);
+  }
+  getSentence() {
+    request.get('/api/sentences')
+           .then((response) => {
+             const sentence = response.body;
+             this.setState({ sentence });
+           });
+  }
+  sendSentence({ body }) {
+    request.post('/api/sentences')
+           .send({ body });
+          //  .then(() => {
+          //    // TODO: How do I fix?
+          //    <Home />
+          //  });
+  }
   render() {
     return (
       <div id="letter">
         <p> Letter is rendering </p>
+        <form onSubmit={this.sendLetter}
+        <Sentence
+          getSentence={this.getSentence}
+          sendSentence={this.sendSentence}
+        />
+        {/* <Sentence />
         <Sentence />
         <Sentence />
-        <Sentence />
-        <Sentence />
-        <Sentence />
+        <Sentence /> */}
+        />
       </div>
     );
   }
--- client/components/member/letter-components/Sentence.jsx
+++ client/components/member/letter-components/Sentence.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 
+const propTypes = {
+  getSentence: React.PropTypes.func,
+  sendSentence: React.PropTypes.func,
+};
+
 class Sentence extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      body: '',
+    };
+    this.handleInputEdit = this.handleInputEdit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleInputEdit(e) {
+    const target = e.target;
+    const name = target.getAttribute('name');
+    const value = target.value;
+
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.sendSentence(this.state);
+  }
   render() {
     return (
       <div id="sentence">
-        <p> Sentence </p>
+        <form onSubmit={this.handleSubmit}
+          <input
+            type="text"
+            name="body"
+            value={this.props.getSentence}
+            onChange={this.handleInputEdit}
+          />
+        />
       </div>
     );
   }
 }
+
+Sentence.propTypes = propTypes;
 
 export default Sentence;
