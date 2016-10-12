c9687108c9c6741125f24cc5abb43e0423ae4c93
Neil Ferner <n.e.ferner@gmail.com>
2016-10-12 11:34:56 -0400

    fix for git pull origin master

--- client/components/guest/game-components/CrankOMeter.jsx
+++ client/components/guest/game-components/CrankOMeter.jsx
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 
 class CrankOMeter extends Component {
-  constructor() {
-    super();
-    this.crank = 0
-  };
-  addCrank () {
-    for ( let i = 0; i <= 15; i ++){
-      
-    }
-  }
-
-
-/* when the user clicks chooses a value we will need to add the number associated with that button to the overall crank score*/
-/*for each question the crank score should be increased by that number */
-  }
   render() {
     return (
       <div id="crank">
